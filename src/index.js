49212328909212ac3cd8d49a2695718eb9cd11fd
tangentguo(郭迅) <tangentguo@tencent.com>
2018-12-17 16:18:56 +0800

    优先使用sendBeacon

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ let Emitter = new EventEmitter()
 let imageElement = new window.Image(1,1)
 let debugColors = require('./lib/colors')
 
-//tool
-let getDataFromDataSet = target=>{
+// tool
+let getDataFromDataSet = target => {
     let data = target.getAttribute('data-ua-trace')
     if (!data) {
         return debug('no data-ua-trace')
@@ -17,102 +17,104 @@ let getDataFromDataSet = target=>{
     return json.parse(data)
 }
 let isObject = (val) => {
-    return val != null && typeof val === 'object' && Array.isArray(val) === false;
+    return val != null && typeof val === 'object' && Array.isArray(val) === false
 }
-//对象=>数组
-let toPairs = obj=>{
+// 对象=>数组
+let toPairs = obj => {
     let arr = []
-    for(let key in obj){
-        arr.push([key,obj[key]])
+    for (let key in obj) {
+        arr.push([key, obj[key]])
     }
     return arr
 }
 
 class UATrace {
-
     constructor (config) {
-        if (!isObject(config)||!config['_url']){
+        if (!isObject(config) || !config['_url']) {
             throw new Error('ua-trace: config object must contain property [_url]')
         }
         this.config = { ...config }
     }
 
-    static version(){
+    static version () {
         return '1.0.0'
     }
 
-    static debug(close){
+    static debug (close) {
         let ls = window.localStorage
-        if(ls){
-            close===false?ls.removeItem('debug'):ls.setItem('debug','ua-trace')
+        if (ls) {
+            close === false ? ls.removeItem('debug') : ls.setItem('debug', 'ua-trace')
         }
     }
 
-    update(config){
+    update (config) {
         this.config = config
     }
-    //数据接收并处理->report
-    subscribe(cb){
-        Emitter.on('reportAll',(data,type)=>{
-            if(!isObject(data)) {
+    // 数据接收并处理->report
+    subscribe (cb) {
+        Emitter.on('reportAll', (data, type) => {
+            if (!isObject(data)) {
                 console.warn('【ua-trace】data-ua-trace value cont parse to json')
             }
-            let newData = cb?cb(data,type):data
-            newData&&this.report(newData,type)
+            let newData = cb ? cb(data, type) : data
+            newData && this.report(newData,type)
         })
     }
 
-    report(obj,type){
-        if ( typeof(obj)==='object' ){
-            let newData = {...this.config,...obj}
-            debug(`%c report：${type||'JS'} `,debugColors.success,newData)
+    report (obj, type) {
+        if (typeof (obj) === 'object') {
+            let newData = { ...this.config, ...obj }
+            debug(`%c report：${type || 'JS'} `, debugColors.success, newData)
             this.imageSrcGet(newData)
         }
     }
 
-    //过滤掉_url,下划线开头为内置变量
-    objToParams(params){
-        let str = '';
+    // 过滤掉_url,下划线开头为内置变量
+    objToParams (params) {
+        let str = ''
         toPairs(params)
             .sort()
-            .map(function(param) {
-                if (param[1] !== undefined&&param[0]!=='_url') {
-                    if (str !== '') str += '&';
-                    str += param[0] + '=' + encodeURIComponent(param[1]);
+            .map(function (param) {
+                if (param[1] !== undefined && param[0] !== '_url') {
+                    if (str !== '') str += '&'
+                    str += param[0] + '=' + encodeURIComponent(param[1])
                 }
-            });
-        return str;
+            })
+        return str
     }
 
-    imageSrcGet(obj){
+    imageSrcGet (obj) {
         obj._rand = new Date().getTime()
-        let src = this.config._url+'?'+this.objToParams(obj)
-        imageElement.src = src
+        let src = this.config._url + '?' + this.objToParams(obj)
+        if (window.navigator) {
+            window.navigator.sendBeacon(src)
+        } else {
+            imageElement.src = src
+        }
     }
-
 }
+
 let selector = '*[data-ua-trace]'
 delegate(selector, 'click', function (e) {
-    //console.log(e)
+    // console.log(e)
     let target = e.delegateTarget
     let data = getDataFromDataSet(target)
-    Emitter.emit('reportAll',data,'click')
+    Emitter.emit('reportAll', data, 'click')
 }, false)
 
-let show = ()=>{
+let show = () => {
     let elms = document.querySelectorAll(selector)
-    Array.prototype.slice.call(elms).map(elm=>{
-        //没曝光过，并且有data-uatrace,可见
-        if(!elm.exposed&&inview(elm)){
+    Array.prototype.slice.call(elms).map( elm => {
+        // 没曝光过，并且有data-uatrace,可见
+        if (!elm.exposed && inview(elm)) {
             let data = getDataFromDataSet(elm)
-            Emitter.emit('reportAll',data, 'expose')
+            Emitter.emit('reportAll', data, 'expose')
             elm.exposed = 1
         }
     })
 }
 
-setTimeout(show,10)
-window.addEventListener('scroll', debounce(show));
+setTimeout(show, 10)
+window.addEventListener('scroll', debounce(show))
 
-
-module.exports  = UATrace
+module.exports = UATrace
